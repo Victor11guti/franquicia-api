8e6b75538b8bb6262a8e377e9f3baa914589aeaa
Victor11guti <victor.guiterrez@correounivalle.edu.co>
2025-05-19 01:17:59 -0500

    configuracion de cors

--- src/main/java/com/accenture/franquicia/config/WebCorsConfig.java
+++ src/main/java/com/accenture/franquicia/config/WebCorsConfig.java
@@ -1,5 +1,7 @@
 package com.accenture.franquicia.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -11,14 +13,14 @@ public class WebCorsConfig {
 
     @Bean
     public CorsWebFilter corsFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOriginPattern("https://*.railway.app");
-        config.addAllowedOriginPattern("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
+    	  CorsConfiguration config = new CorsConfiguration();
+          config.setAllowCredentials(true);
+          config.setAllowedOrigins(List.of("https://franquicia-api-production.up.railway.app"));
+          config.addAllowedHeader("*");
+          config.addAllowedMethod("*");
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-        return new CorsWebFilter(source);
+          UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+          source.registerCorsConfiguration("/**", config);
+          return new CorsWebFilter(source);
     }
 }
